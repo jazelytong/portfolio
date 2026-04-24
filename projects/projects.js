32e95afe112e887b5dd850ebfb439bb081ba3bdd
Jazely Tong <j8tong@ucsd.edu>
2026-04-23 22:47:02 -0700

    latest projects

---
+++ projects/projects.js
@@ -0,0 +1,10 @@
+import { fetchJSON, renderProjects } from '../global.js';
+
+const projects = await fetchJSON('../lib/projects.json');
+
+const titleElement = document.querySelector('.projects-title');
+titleElement.textContent = `${projects.length} Projects`;
+
+const projectsContainer = document.querySelector('.projects');
+
+renderProjects(projects, projectsContainer, 'h2');
